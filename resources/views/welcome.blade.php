284ee5e2d4994b6aabaa676c806138310dd2c5a6
Juba <abdelrahman.fawzy@brimore.com>
2022-09-14 22:00:50 +0200

    Update Composer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,7 @@
     </footer>
 </div>
 
-@routes
+@routes()
 @vite('resources/js/app.js')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
